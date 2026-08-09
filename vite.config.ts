46a37f4b860f3afe8d5a894e73e08de9703639ed
Skelrin <maxime.herbin0545@laposte.net>
2026-08-09 19:05:37 +0200

    fix : typo on manifest

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
         background_color: '#f8fafc',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
-        scope: '/',
+        start_url: './',
+        scope: './',
         icons: [
           {
             src: 'pwa-192x192.png',
